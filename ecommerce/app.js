1e8d99ba80d5bec3aeb8554fbc962fa3d6b025da
Andres <andrescardenas34@outlook.com>
2020-02-01 17:15:04 -0500

    new project

---
+++ ecommerce/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require('mongoose')
+require("dotenv").config();
+
+//import routes
+const userRoutes = require('./routes/user')
+
+//app
+const app = express();
+
+//database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+}).then(()=> console.log('DB CONNECTED'))
+
+//routes middleware
+app.use("/api", userRoutes);
+
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+console.log(`server running on ${port}`)
+});
---
+++ ecommerce/models/user.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose');
+// const crypto = require('crypto');
+// const uuidv1 = require('uuid/v1');
+
+
+// const userSchema = new mongoose.Schema({
+
+// })
